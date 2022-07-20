6a23cfafd756e4105fd754b99aac1abd24e2cb3d
Keith Kreschollek <96690604+spike9743@users.noreply.github.com>
2022-07-19 22:38:18 -0400

    Delete main directory

--- main/java/net/fabricmc/amethystsong/gui/PortalDestination.java
+++
@@ -1,8 +0,0 @@
-package net.fabricmc.amethystsong.gui;
-
-import io.github.cottonmc.cotton.gui.widget.WButton;
-import io.github.cottonmc.cotton.gui.widget.WLabel;
-import io.github.cottonmc.cotton.gui.widget.WPlainPanel;
-import net.minecraft.text.Text;
-
-
